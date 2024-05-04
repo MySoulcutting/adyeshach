f479598e438d5d74386b2c04b4a56ceca9a20bc4
White_Souls <72398605+MySoulcutting@users.noreply.github.com>
2024-05-04 13:32:25 +0800

    Update build.gradle.kts

--- project/compat-dragon-core/build.gradle.kts
+++ project/compat-dragon-core/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    compileOnly("public:DragonCore:2.6.2.9")
+    compileOnly(fileTree("libs"))
     compileOnly(project(":project:common"))
     compileOnly(project(":project:common-impl"))
 }
